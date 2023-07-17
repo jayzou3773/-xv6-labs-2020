70b0caeb4dba6ad001a3f5fec2d549820b88ecd1
jay <1743940094@qq.com>
2023-07-17 15:31:13 +0800

    labs1

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc!=2)
+    {
+        write(2,"Usage: sleep time\n",strlen("Usage: sleep time\n"));
+        exit(1);
+    }
+
+    int time =atoi(argv[1]);
+
+    sleep(time);
+    printf("nothing happens for a little while\n");
+    exit(0);
+}
